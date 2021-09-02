18b95c4a425abcc47baef2502147f5700b8ed7e9
duchangkim <duchang.dev@gmail.com>
2021-09-02 22:23:51 +0900

    Feat: Commit first

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,69 @@
+<div class="field">
+  <div
+    class="control has-icons-left has-icons-right todo-item"
+    class:todo-item--checked={todo.checked}>
+    <input
+      class="input"
+      type="text"
+      {placeholder}
+      bind:value={todo.text}
+      on:change="{(e) => onHandleModify(e, todo.id)}"
+    />
+    <span 
+      class="icon is-small is-left"
+      on:click="{() => onHandleCheck(todo.id)}"
+    >
+      <i class="fas fa-check" />
+    </span>
+    <span 
+      class="icon is-small is-right"
+      on:click="{() => onHandleRemove(todo.id)}"
+    >
+      <i class="fas fa-trash" />
+    </span>
+  </div>
+</div>
+
+<script lang="ts">
+  import type {
+    ITodoItem, 
+    IHandleCheck, 
+    IHandleRemove, 
+    IHandleModify,
+  } from '../interfaces';
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  export let todo: ITodoItem;
+  export let onHandleCheck: IHandleCheck;
+  export let onHandleRemove: IHandleRemove;
+  export let onHandleModify: IHandleModify;
+
+  let placeholder = "할 일을 입력해주세요";
+</script>
+
+<style lang="scss">
+  .control.has-icons-left,
+  .control.has-icons-right {
+    & .icon {
+      pointer-events: inherit;
+      transition: color 0.5s;
+      cursor: pointer;
+
+      &:hover {
+        color: #363636;
+      }
+    }
+
+    & .input:focus ~ .icon {
+      color: #dbdbdb;
+    }
+  }
+
+  .todo-item {
+    &--checked {
+      opacity: 0.4;
+    }
+  }
+</style>
+
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,24 @@
+<div>
+  {#if remaining}
+    <p>남은 일이 {remaining}개 남았어요</p>
+  {:else}
+    <p>남은 일이 없어요</p>
+  {/if}
+  <br />
+  {#each todos as todo (todo.id)}
+    <TodoItem {todo} {onHandleCheck} {onHandleRemove} {onHandleModify} />
+  {/each}
+</div>
+
+<script lang="ts">
+  import TodoItem from "./TodoItem.svelte";
+  import type { ITodoItem, IHandleCheck, IHandleRemove, IHandleModify } from '../interfaces';
+
+  export let todos: Array<ITodoItem>;
+  export let onHandleCheck: IHandleCheck;
+  export let onHandleRemove: IHandleRemove;
+  export let onHandleModify: IHandleModify;
+
+  $: remaining = todos.filter((t) => !t.checked).length;
+</script>
+
